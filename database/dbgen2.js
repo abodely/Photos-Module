3bbd46bed8e0aa93bc933b67d6fcc6d9620c08ec
Jia <jiafeng3124@gmail.com>
2019-04-19 20:42:03 -0700

    create 10 million home listings and 150 million photos

---
+++ database/dbgen2.js
@@ -0,0 +1,57 @@
+const faker = require('faker');
+const fs = require('fs');
+const path = require('path');
+
+const getRandomId = () => {
+  return Math.floor(Math.random() * 1033 + 1);
+}
+
+const convertToCSV =  (data) => {
+  let rows = "";
+  for (let key in data) {
+    rows += data[key] + ',';
+  };
+  rows = rows.slice(0, rows.length-1);
+  rows += '\n';
+  return rows
+}
+
+const createMockDataFile = () => {
+  const homeFile = fs.createWriteStream(path.join(__dirname, 'homeData.csv'));
+  const photoFile = fs.createWriteStream(path.join(__dirname, 'photosData.csv'));
+  (async() => {
+      let counter = 0;
+      for (let i = 1; i < 10000000; i++) {
+        let homeData = { id: i };
+        if (counter === 0) {
+          let header = 'id\n';
+          homeFile.write(header);
+          counter = 1;
+        }
+        let homeCSV = convertToCSV(homeData)
+        if (!homeFile.write(homeCSV)) {
+            await new Promise(resolve => homeFile.once('drain', resolve));
+        }
+      }
+      counter = 0;
+      for (let i = 1; i < 150000000; i++) {
+        let photo = {};
+        photo.id = i;
+        photo.url = `https://s3.amazonaws.com/sdc-airbnb-photos/photo${getRandomId()}.jpg`;
+        photo.comment = faker.lorem.words();
+        photo.home_id = Math.floor(Math.random() * 1000 + 1);
+        if (counter === 0) {
+          let header = "id,url,comment,home_id\n";
+          photoFile.write(header);
+          counter = 1;
+        } else {
+          let photoCSV = convertToCSV(photo);
+          if (!photoFile.write(photoCSV)) {
+              await new Promise(resolve => photoFile.once('drain', resolve));
+          }
+        }
+      }
+})();
+}
+
+createMockDataFile();
--- database/index.js
+++ database/index.js
@@ -37,31 +37,3 @@ Home.sync({ force: true });
 Photo.sync({ force: true });
 
 module.exports = { Home, Photo };
-
-// const mongoose = require('mongoose');
-// const { DBUSER, DBPW } = require('./dbAccessKey');
-
-
-// const db = mongoose.connect(`mongodb://${DBUSER}:${DBPW}@localhost/photosAndComments`, { useNewUrlParser: true });
-// // const db = mongoose.connect('mongodb://172.17.0.2/photosAndComments', { useNewUrlParser: true, useCreateIndex: true });
-
-// const photosAndCommentsSchema = mongoose.Schema([
-//   {
-//     id: { type: Number, unique: true },
-//     photosAndComments: [
-//       {
-//         url: String,
-//         comment: String,
-//       },
-//     ],
-//   },
-// ]);
-
-// const photosAndComments = mongoose.model('photosAndComments', photosAndCommentsSchema);
-
-
-// module.exports = {
-//   photosAndComments,
-//   db,
-//   photosAndCommentsSchema,
-// };
