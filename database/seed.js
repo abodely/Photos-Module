a652beb340c8dd433c296ccc139d050a866cd27d
Jia <jiafeng3124@gmail.com>
2019-04-22 19:13:38 -0700

    generate seeding script for postgres

--- database/seed.js
+++ database/seed.js
@@ -1,4 +1,10 @@
-const homeData = require('./homeData.csv');
-const photosData = require('./photosData.csv');
+const sequelize = require('./index.js');
 const { Home, Photo } = require('./model.js');
 
+Home.sync({ force: true })
+  .then(() => sequelize.query(`COPY homes FROM '/Users/jfeng/Documents/SDC/photos_module/database/homeData.csv' DELIMITER ',' CSV HEADER`))
+  .catch(err => console.log(err, 'sync error'));
+
+Photo.sync({ force: true })
+  .then(() => sequelize.query(`COPY photos FROM '/Users/jfeng/Documents/SDC/photos_module/database/photosData.csv' DELIMITER ',' CSV HEADER`))
+  .catch(err => console.log(err, 'sync error'));
