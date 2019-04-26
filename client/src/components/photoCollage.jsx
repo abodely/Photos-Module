1b289a87b94a0c049d800cf0071a2319d43cf4fc
Jia <jiafeng3124@gmail.com>
2019-04-26 16:16:19 -0700

    implement get request route

--- client/src/components/photoCollage.jsx
+++ client/src/components/photoCollage.jsx
@@ -5,11 +5,11 @@ import styles from '../styles/photoCollage.css';
 const PhotoCollage = ({
   dataArr, handleClickedImage, toggleModal,
 }) => {
-  if (dataArr.photosAndComments.length < 5) {
+  if (dataArr.length < 5) {
     return (
       <div>
         <div className={styles.containerLessThan5}>
-          <img className={styles.firstImageLessThan5} src={dataArr.photosAndComments[0].imageUrl} onClick={() => {handleClickedImage(0, () => {toggleModal()})}} alt="host home" />
+          <img className={styles.firstImageLessThan5} src={dataArr[0].imageUrl} onClick={() => {handleClickedImage(0, () => {toggleModal()})}} alt="host home" />
         </div>
       </div>
     );
@@ -18,19 +18,19 @@ const PhotoCollage = ({
     <div>
       <div className={styles.mainContainer}>
         <div className={styles.firstImageContainer}>
-          <img className={styles.firstImage} src={dataArr.photosAndComments[0].imageUrl} onClick={() => {handleClickedImage(0, () => {toggleModal()})}} alt="host home" />
+          <img className={styles.firstImage} src={dataArr[0].imageUrl} onClick={() => {handleClickedImage(0, () => {toggleModal()})}} alt="host home" />
         </div>
         <div className={styles.secondImageContainer}>
-          <img className={styles.secondImage} src={dataArr.photosAndComments[1].imageUrl} onClick={() => {handleClickedImage(1, () => {toggleModal()})}} alt="host home" />
+          <img className={styles.secondImage} src={dataArr[1].imageUrl} onClick={() => {handleClickedImage(1, () => {toggleModal()})}} alt="host home" />
         </div>
         <div className={styles.thirdImageContainer}>
-          <img className={styles.thirdImage} src={dataArr.photosAndComments[2].imageUrl} onClick={() => {handleClickedImage(2, () => {toggleModal()})}} alt="host home" />
+          <img className={styles.thirdImage} src={dataArr[2].imageUrl} onClick={() => {handleClickedImage(2, () => {toggleModal()})}} alt="host home" />
         </div>
         <div className={styles.fourthImageContainer}>
-          <img className={styles.fourthImage} src={dataArr.photosAndComments[3].imageUrl} onClick={() => {handleClickedImage(3, () => {toggleModal()})}} alt="host home" />
+          <img className={styles.fourthImage} src={dataArr[3].imageUrl} onClick={() => {handleClickedImage(3, () => {toggleModal()})}} alt="host home" />
         </div>
         <div className={styles.fifthImageContainer}>
-          <img className={styles.fifthImage} src={dataArr.photosAndComments[4].imageUrl} onClick={() => {handleClickedImage(4, () => {toggleModal()})}} alt="host home" />
+          <img className={styles.fifthImage} src={dataArr[4].imageUrl} onClick={() => {handleClickedImage(4, () => {toggleModal()})}} alt="host home" />
         </div>
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 const app = express();
 const PORT = 3001;
 const sequelize = require('../postgresdb/index');
-const { Home, Photo } = require('../postgresdb/model');
+const { Photo } = require('../postgresdb/model');
 
 app.use(morgan('dev'));
 app.use(compression());
@@ -15,61 +15,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', express.static(path.join(__dirname, '../public')));
 app.use('/home/:homeid/photos', express.static(path.join(__dirname, '../public')));
 
-
 app.get('/api/home/:homeid/photos', (req, res) => {
-  console.time('response time: ')
-  let getQuery = `SELECT * FROM photos WHERE home_id = ${req.params.homeid};`;
-  sequelize.query(getQuery, { type: sequelize.QueryTypes.SELECT })
+  sequelize.query(`SELECT * FROM photos WHERE home_id = ${req.params.homeid};`, { type: sequelize.QueryTypes.SELECT })
     .then(data => {
     res.json(data); 
-    console.timeEnd('response time: ')
-  })
+  });
 });
-  
 
+app.post('/api/home/:homeid/photos', (req, res) => {
+  Photo.save()
+});
 
-// app.post('/api/home/:homeid/photos/add', (req, res) => {
-//   index.photosAndComments.save({ id: parseInt(req.params.id, 10) }, { _id: 0 })
-//     .select('photosAndComments')
-//     .exec((err, data) => {
-//       if (err) {
-//         console.log('ERROR finding data from db: ', err);
-//       } else {
-//         res.setHeader('Content-Type', 'application/json');
-//         res.json(data);
-//       }
-//     });
-// });
+app.patch('/api/home/:homeid/photos', (req, res) => {
+});
 
-// app.updateOne('/api/home/:homeid/photos/update', (req, res) => {
-//   index.photosAndComments.updateOne({ id: parseInt(req.params.id, 10) }, { _id: 0 })
-//     .select('photosAndComments')
-//     .exec((err, data) => {
-//       if (err) {
-//         console.log('ERROR finding data from db: ', err);
-//       } else {
-//         res.setHeader('Content-Type', 'application/json');
-//         res.json(data);
-//       }
-//     });
-// });
-
-// app.delete('/api/home/:homeid/photos/delete', (req, res) => {
-//   index.photosAndComments.deleteOne({ id: parseInt(req.params.id, 10) }, { _id: 0 })
-//     .select('photosAndComments')
-//     .exec((err, data) => {
-//       if (err) {
-//         console.log('ERROR finding data from db: ', err);
-//       } else {
-//         res.setHeader('Content-Type', 'application/json');
-//         res.json(data);
-//       }
-//     });
-// });
-
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/index.html'));
-// });
+app.delete('/api/home/:homeid/photos', (req, res) => {
+});
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
